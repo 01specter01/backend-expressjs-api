c22ba40ee808fe1982c01af5217c11db479cc52e
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-29 14:57:51 +0100

    1 done

--- server.mjs
+++ server.mjs
@@ -1,18 +1,11 @@
-import data from "./data.json";
+import data from "./data.json" assert { type: "json" };
 import express from "express";
 
 const app = express();
-const output = {
-    <ul>
-        {data.map(( key, id) => (
-            <li key={}>{id}</li>
-        ))}
-    </ul>
-}
+
 const server = app.listen(3011, () => {
-    
     console.log("start");
 });
 app.get("/", (request, response) => {
-    response.send(`${server}`);
+    response.send(data);
 });
