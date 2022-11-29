d3f03feecc44cc73ed679927fc5519d54492d23f
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-29 14:33:21 +0100

    h

--- server.mjs
+++ server.mjs
@@ -2,19 +2,17 @@ import data from "./data.json";
 import express from "express";
 
 const app = express();
-
-function dataList({ ...data }) {
-    {
-        <ul>
-            {data.map((s) => (
-                <li>{s}</li>
-            ))}
-        </ul>;
-    }
+const output = {
+    <ul>
+        {data.map(( key, id) => (
+            <li key={}>{id}</li>
+        ))}
+    </ul>
 }
 const server = app.listen(3011, () => {
+    
     console.log("start");
 });
 app.get("/", (request, response) => {
-    response.send(`${dataList}`);
+    response.send(`${server}`);
 });
