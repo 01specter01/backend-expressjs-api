01923234a218b5449ee58952c65d2964c225ddf1
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-29 19:21:38 +0100

    3

--- server.mjs
+++ server.mjs
@@ -6,6 +6,14 @@ const app = express();
 const server = app.listen(3011, () => {
     console.log("start");
 });
-app.get("/", (request, response) => {
-    response.send(data);
+app.get ("/user", (request, response) => {
+    response.json()
+})
+app.get("/user/:id", (request, response) => {
+    const result = user.find((el) => el.id === +request.params.id);
+
+    if (result) {
+        return response.json(result);
+    }
+    response.json("No user with this id");
 });
